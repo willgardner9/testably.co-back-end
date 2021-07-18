03b838b84e5c213f67cf0576cbf9e37a162c6ac1
willgardner9 <willgardner9@gmail.com>
2021-07-18 12:23:31 +0100

    Cors options update

--- app.js
+++ app.js
@@ -30,11 +30,11 @@ app.use(express.json());
 // ** ENABLE CORS ** //
 const cors = require('cors');
 
-// const corsOptions = {
-//   origin: 'https://testably-co-front-end-fn4kh7lb5-willgardner9.vercel.app/',
-//   credentials: true,
-// };
-app.use(cors());
+const corsOptions = {
+  origin: 'https://testably-co-front-end.vercel.app/',
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 //  **  COOKIE PARSER MIDDLEWARE  **  //
 const cookieParser = require('cookie-parser');
